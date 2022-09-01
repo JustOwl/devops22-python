266241970a748ee295453d694c813aa2c392df28
JustOwl <43913825+JustOwl@users.noreply.github.com>
2022-09-01 14:29:07 +0200

    Made print and input files

---
+++ lesson_2/JustOwl/input.py
@@ -0,0 +1,14 @@
+from ctypes.wintypes import PINT
+
+
+new_string = input ("String:") 
+new_float = input("Number:")
+
+#for item in range(10):
+#    print(new_string)
+
+print(f'{new_string}{new_string}')
+
+divide_int = float(new_float) / 3.5
+
+print(divide_int)
---
+++ lesson_2/JustOwl/print.py
@@ -0,0 +1,8 @@
+whole_number = 69
+floating_number = 69.69
+text_string = "!this is a string!"
+boleon_value = True
+none_value = None
+
+print(f'{whole_number} {floating_number} {text_string} {boleon_value} {none_value}')
+
