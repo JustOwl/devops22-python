07627b407cb9c7603e742393a3081b88af5859b4
JustOwl <ka03bj@gmail.com>
2022-09-09 09:30:39 +0200

    extra salary övning

---
+++ lesson_5/JustOwl/salary.py
@@ -0,0 +1,26 @@
+user_name = "Kalle"
+salary = 10000
+flag = False
+
+print(f'{user_name} your current salary is {salary}')
+
+
+new_salary = input("How much would you like to increase it by?")
+print(f'Haha \N{face with tears of joy} No')
+
+while flag == False:
+    new_salary = input("How much would you like to increase it by?")
+
+    if((int(new_salary)/int(salary)) > 0.05):
+        print("No thats too much")
+    else:
+        flag = True
+        break
+
+if(flag):
+    print("Ok that looks good")
+    salary += int(new_salary)
+    print(f'This is your new salary: {salary}')
+
+if(flag == False):
+    print("Im sorry but your offers were too high")
