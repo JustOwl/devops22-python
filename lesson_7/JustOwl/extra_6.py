7fd1ea2145d23e2585e0f687005ab8e779ebd270
JustOwl <43913825+JustOwl@users.noreply.github.com>
2022-09-14 13:40:16 +0200

    Update extra_6.py

--- lesson_7/JustOwl/extra_6.py
+++ lesson_7/JustOwl/extra_6.py
@@ -1,16 +1,15 @@
 def user_input():
-    return input("Please input two numbers and an operator(+,-,*,/,%): \n Input format: first number,secondnumber,operator:").split(",")
+    return input("Please input two numbers and an operator(+  - * / %): \n Input format: first number,secondnumber,operator:").split(",")
 
-def lambda_calc(x,y,operator):
-    lambda_dict = {"+":lambda x: x+y,
-                   "-":lambda x: x-y,
-                   "*":lambda x: x*y,
-                   "/":lambda x: x/y,
-                   "%":lambda x: x%y}
+def lambda_calc(operator):
+    lambda_dict = {"+":lambda x,y: x+y,
+                   "-":lambda x,y: x-y,
+                   "*":lambda x,y: x*y,
+                   "/":lambda x,y: x/y,
+                   "%":lambda x,y: x%y}
     return lambda_dict[operator]
 
 a,b,c = user_input()
 
-test = lambda_calc(a,b,c)
-print(int(test))
-
+test = lambda_calc(c)
+print(test(float(a),float(b)))
